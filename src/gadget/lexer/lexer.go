17187685fba4907d90cd545015cff21b8483732a
Vishav kandola <vkandola@hotmail.com>
2017-09-26 21:45:18 -0700

    Add Lexer struct and New function for it

--- src/gadget/lexer/lexer.go
+++ src/gadget/lexer/lexer.go
@@ -1,1 +1,13 @@
 package lexer
+
+type Lexer struct {
+	input			string
+	position		int		// current position in input (points to current char)
+	readPosition	int		// current reading position in input (after current char)
+	ch				byte	// current char being looked at
+}
+
+func New(input string) *Lexer {
+	l := &Lexer{ input: input }
+	return l
+}
