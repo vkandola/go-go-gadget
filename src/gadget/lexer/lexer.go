c25505c266f3eec3ba72681596e05e3617d34dae
Vishav kandola <vkandola@hotmail.com>
2017-10-01 21:06:39 -0700

    Add readChar helper.

--- src/gadget/lexer/lexer.go
+++ src/gadget/lexer/lexer.go
@@ -11,3 +11,13 @@ func New(input string) *Lexer {
 	l := &Lexer{ input: input }
 	return l
 }
+
+func (l *Lexer) readChar() {
+	if l.readPosition >= len(l.input) {
+		l.ch = 0
+	} else {
+		l.ch = l.input[l.readPosition]
+	}
+	l.position = l.readPosition
+	l.readPosition += 1
+}
